328ed2a95d5b5be3ed98e4919101c083f8d5019f
Eytan zichel <eytanzichel@gmail.com>
2024-09-19 17:07:26 +0300

    save data in database

--- app.py
+++ app.py
@@ -4,17 +4,20 @@ from blue_prints import players_bp
 from db import db
 from blue_prints.players_bp import player_bp
 from blue_prints.team_bp import team_bp
+from services.load_NBA_data import grid_data
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///version01.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///version06.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+players = grid_data(2022, 2024)
 
 db.init_app(app)
-
 with app.app_context():
     db.create_all()
+    db.session.add_all(players)
+    db.session.commit()
 
 @app.route('/')
 def index():
